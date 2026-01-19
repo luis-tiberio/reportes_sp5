dfdf08eef08c3ba05074bc38642607ba01616c0e
luis-tiberio <luisotaviotiberio@gmail.com>
2026-01-19 16:42:00 -0300

    Update main_script_imagem.py

--- main_script_imagem.py
+++ main_script_imagem.py
@@ -375,10 +375,9 @@ async def main():
             await browser.close()
 
     # UPLOAD E LÓGICA LOCAL
-    if final_path1 and final_path2 and final_path3:
+    if final_path1 and final_path2:
         update_sheet(final_path1, ID_PLANILHA_DADOS, "PROD")
         update_sheet(final_path2, ID_PLANILHA_DADOS, "WS T1")
-        #update_sheet(final_path3, ID_PLANILHA_INBOUND, "INBOUND")
         print("Sincronizando (20s)...")
         time.sleep(20)
 
